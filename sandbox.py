6e738f609832b63fa54f2e186925f3d8d41470f4
Chris Weller <chriskweller@gmail.com>
2015-05-21 00:03:11 -0700

    Blowfish encryption example.

---
+++ sandbox.py
@@ -0,0 +1,25 @@
+ # -*- coding: utf-8 -*-
+
+from Crypto.Cipher import Blowfish
+
+INPUT_SIZE = 8
+SSN_KEY = '123456789987654321'
+ 
+def pad_string(str):
+    new_str = str
+    pad_chars = INPUT_SIZE - (len(str) % INPUT_SIZE)
+ 
+    if pad_chars != 0:
+        for x in range(pad_chars):
+            new_str += " "
+                
+    return new_str
+ 
+plaintext = "DushyantSrikant"
+crypt_obj = Blowfish.new(SSN_KEY, Blowfish.MODE_ECB)
+ciphertext = crypt_obj.encrypt(pad_string(plaintext))
+ 
+print "Plaintext: " + plaintext
+print "Blowfish Cyphertext: " + ciphertext
+print "Back to Plaintext: " + crypt_obj.decrypt(ciphertext)
+

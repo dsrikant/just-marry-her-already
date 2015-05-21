22b1ab49b5d342cf6432fb4af33309d9b0f223c9
Dushyant Srikant <dushyant.srikant@gmail.com>
2015-05-21 01:53:19 -0700

    Got some letters

--- nameGen.py
+++ nameGen.py
@@ -1,4 +1,5 @@
- # -*- coding: utf-8 -*-
+import string 
+# -*- coding: utf-8 -*-
 from Crypto.Cipher import Blowfish
 INPUT_SIZE = 8
 SSN_KEY = '123456789987654321'
@@ -13,7 +14,34 @@ def pad_string(str):
                 
     return new_str
  
-plaintext = "DushyantSrikant"
+
+def bro1(y):
+  h = 0
+  inc = 97
+  while h < 96:
+      if h == y:
+          return inc
+      else:
+          inc+=1
+          h+=1
+      if inc == 123:
+          inc = 97
+
+
+def bro2(y):
+    h = 0
+    inc = 97
+    while h < 256:
+        if h == y:
+            return inc
+        else:
+            inc+=1
+            h+=1
+        if inc == 123:
+            inc = 97
+
+
+plaintext = "SrikantCabezas"
 crypt_obj = Blowfish.new(SSN_KEY, Blowfish.MODE_ECB)
 ciphertext = crypt_obj.encrypt(pad_string(plaintext))
 
@@ -22,9 +50,31 @@ print var
 
 ohye = var.split(":")
 
+new = []
+
+alph = list(string.ascii_lowercase)
+
 for i in ohye:
-    print int(i, 16)
-    
+    j = int(i, 16)
+    print j
+    new.append(j)
+
+name = []
+
+for y in new:
+    if y < 97:
+        name.append(bro1(y))
+    elif y > 122:
+        name.append(bro2(y))
+    else:
+        name.append(y)
+
+print "Huge fucking blank::::::::::::::::"
+
+for helo in name:
+    print str(unichr(helo))
+
+name = []
 
 print "Plaintext: " + plaintext
 print "Blowfish Cyphertext: " + ciphertext.encode('hex')
